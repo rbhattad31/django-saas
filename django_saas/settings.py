beb2d732f39f4553f7afdf2e4228bfab44cab53a
bradsolgopi <gopi.dammu@bradsol.com>
2025-08-13 15:16:08 +0530

    Peopertychanges and sales deal reference number DB changes

--- django_saas/settings.py
+++ django_saas/settings.py
@@ -98,22 +98,22 @@ WSGI_APPLICATION = 'django_saas.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default':  {
-    #     'ENGINE': 'django.db.backends.mysql',  # or 'sqlite3', 'mysql', etc.
-    #     'NAME': os.getenv('DATABASE_NAME'),  # Default database name
-    #     'USER': os.getenv('DATABASE_USER'),
-    #     'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-    #     'HOST': os.getenv('DATABASE_HOST'),
-    #     'PORT': os.getenv('DATABASE_PORT', '3306'),
-    # }
-       'default':  {
+    'default':  {
         'ENGINE': 'django.db.backends.mysql',  # or 'sqlite3', 'mysql', etc.
-        'NAME':  "gopitest",  # Default database name
-        'USER':  "root",
-        'PASSWORD': "root",
-        'HOST': "localhost",
-        'PORT': "3306",
+        'NAME': os.getenv('DATABASE_NAME'),  # Default database name
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', '3306'),
     }
+    #    'default':  {
+    #     'ENGINE': 'django.db.backends.mysql',  # or 'sqlite3', 'mysql', etc.
+    #     'NAME':  "gopitest",  # Default database name
+    #     'USER':  "root",
+    #     'PASSWORD': "root",
+    #     'HOST': "localhost",
+    #     'PORT': "3306",
+    # }
 }
 
 
