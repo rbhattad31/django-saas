18fc622e9f755e8deb89fd6a579791c3b32028f0
naga gopi dammu <gopi.dammu@bradsol.com>
2025-10-10 05:05:33 +0530

    changes the new and renewal2.0

--- Rental_Deal/views.py
+++ Rental_Deal/views.py
@@ -205,7 +205,7 @@ class Rental_DealViewSet(viewsets.ModelViewSet):
 
         # Field-specific filters
         filter_fields = [
-            "reference_number", "unit_details", "building_name", "is_deal_new",
+            "reference_number", "unit_details", "building_name", "is_new_deal",
             "is_approved_rejected", "project_name", "owner_first_name", "tenant_first_name",
             "owner_mobile", "tenant_mobile"
         ]
